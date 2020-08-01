364eaed11ab74c33ac8de56e74ba1b1493d380a5
Michael Dresser <michaelmdresser@gmail.com>
2020-07-31 19:13:21 -0600

    Removed license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject teemo-tool "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "none"
+            :url "none"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [irclj "0.5.0-alpha4"]
                  [org.clojure/java.jdbc "0.7.11"]
